5d0f1ff54a92454cc623fe72c7311805725b3f44
duranje <joeduran8@gmail.com>
2018-02-20 14:52:31 -0600

    complete read-only version of feature switch fetcher;

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,63 +1,144 @@
 module Main exposing (..)
+
+-- comonent import example
+
+import Decoders.FeatureSwitches exposing (decodeFeatureSwitches)
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing ( onClick )
-
--- component import example
-import Components.Hello exposing ( hello )
+import Http
+import Model.FeatureSwitches exposing (FeatureSwitch)
 
 
 -- APP
+
+
 main : Program Never Model Msg
 main =
-  Html.beginnerProgram { model = model, view = view, update = update }
+    Html.program { init = init, view = view, update = update, subscriptions = subscriptions }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( initialModel, featureSwitchRequest )
+
 
 
 -- MODEL
-type alias Model = Int
 
-model : Model
-model = 0
+
+type alias Model =
+    { featureSwitches : List FeatureSwitch
+    , error : Maybe String
+    }
+
+
+initialModel : Model
+initialModel =
+    { featureSwitches = []
+    , error = Nothing
+    }
+
 
 
 -- UPDATE
-type Msg = NoOp | Increment
 
-update : Msg -> Model -> Model
+
+type Msg
+    = FetchFeatureSwitches
+    | FetchedFeatureSwitches (Result Http.Error (List FeatureSwitch))
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    NoOp -> model
-    Increment -> model + 1
+    case msg of
+        FetchFeatureSwitches ->
+            ( initialModel, featureSwitchRequest )
+
+        FetchedFeatureSwitches (Ok newFeatureSwitches) ->
+            ( Model newFeatureSwitches Nothing, Cmd.none )
+
+        FetchedFeatureSwitches (Err _) ->
+            ( { model | error = Just "something happened!" }, Cmd.none )
+
 
 
 -- VIEW
 -- Html is defined as: elem [ attribs ][ children ]
 -- CSS can be applied via class names or inline style attrib
+
+
 view : Model -> Html Msg
 view model =
-  div [ class "container", style [("margin-top", "30px"), ( "text-align", "center" )] ][    -- inline CSS (literal)
-    div [ class "row" ][
-      div [ class "col-xs-12" ][
-        div [ class "jumbotron" ][
-          img [ src "static/img/elm.jpg", style styles.img ] []                             -- inline CSS (via var)
-          , hello model                                                                     -- ext 'hello' component (takes 'model' as arg)
-          , p [] [ text ( "Elm Webpack Starter" ) ]
-          , button [ class "btn btn-primary btn-lg", onClick Increment ] [                  -- click handler
-            span[ class "glyphicon glyphicon-star" ][]                                      -- glyphicon
-            , span[][ text "FTW!" ]
-          ]
+    let
+        containerBody =
+            case model.error of
+                Just error ->
+                    errorView error
+
+                Nothing ->
+                    featureSwitchesView model.featureSwitches
+    in
+    div [ class "container", style [ ( "margin-top", "30px" ), ( "text-align", "center" ) ] ] [ containerBody ]
+
+
+featureSwitchView : FeatureSwitch -> Html Msg
+featureSwitchView featureSwitch =
+    li [ class (featureSwitchClass featureSwitch), style styles.featureSwitch ]
+        [ span [ class "name" ] [ text featureSwitch.name ]
+        , span [ class "description" ] [ text featureSwitch.description ]
         ]
-      ]
-    ]
-  ]
+
+
+featureSwitchesView : List FeatureSwitch -> Html Msg
+featureSwitchesView featureSwitches =
+    ul []
+        (List.map
+            featureSwitchView
+            featureSwitches
+        )
+
+
+errorView : String -> Html Msg
+errorView errorMessage =
+    div [ class "error" ] [ text errorMessage ]
+
 
 
 -- CSS STYLES
-styles : { img : List ( String, String ) }
+
+
+featureSwitchClass : FeatureSwitch -> String
+featureSwitchClass featureSwitch =
+    if featureSwitch.isOn then
+        "feature-switch on"
+    else
+        "feature-switch"
+
+
+styles : { featureSwitch : List ( String, String ) }
 styles =
-  {
-    img =
-      [ ( "width", "33%" )
-      , ( "border", "4px solid #337AB7")
-      ]
-  }
+    { featureSwitch =
+        [ ( "display", "flex" ), ( "flex-direction", "column" ) ]
+    }
+
+
+
+-- HTTP
+
+
+featureSwitchRequest : Cmd Msg
+featureSwitchRequest =
+    let
+        url =
+            "http://localhost:8888/features"
+    in
+    Http.send FetchedFeatureSwitches (Http.get url decodeFeatureSwitches)
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
